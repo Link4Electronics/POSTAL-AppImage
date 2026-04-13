54fce00b54dbfbd772969b527336707565b20aba
Link4Electronics <link4electronics@gmail.com>
2026-04-13 03:39:51 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -29,5 +29,5 @@ mkdir -p ./AppDir/bin
 cd ./POSTAL
 patch -Np1 -i ../fixes.patch
 make -j$(nproc)
-mv -v postal1-x86_64 ../AppDir/bin
+mv -v bin/postal1-x86_64 ../AppDir/bin
 mv -v DefaultPostal.ini ../AppDir/bin/POSTAL.INI
